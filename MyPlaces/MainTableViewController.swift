33948d5ae5f7978d9a131679866be28a4d5b5fef
Evgeniy <sea.85@mail.ru>
2022-09-14 22:08:31 +0300

    In this moment I don't understand anymore

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -6,56 +6,71 @@
 //
 
 import UIKit
+import RealmSwift
 
 class MainTableViewController: UITableViewController {
-
-    let restaurantNames = [
-    "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
-    "Индокитай", "X.O", "Балкан Гриль", "Sherlok Holmes",
-    "Speak Easy", "Morris Pub", "Вкусные истории",
-    "Классик", "Love&Life", "Шок", "Бочка"
-    ]
+    
+    var places: Results<Place>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        places = realm.objects(Place.self)
     }
     
-   
-
+    
+    
     // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
-    }
-
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-
-        cell.nameLabel.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
-        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height≈ / 2
-        cell.imageOfPlace.clipsToBounds = true
-        
-        return cell
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.isEmpty ? 0 : places.count
     }
+    
+    
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+    
+            let place = places[indexPath.row]
+    
+            cell.nameLabel.text = place.name
+            cell.locationLabel.text = place.location
+            cell.typeLabel.text = place.type
+            cell.imageOfPlace.image = UIImage(data: place.imageData!)
+            cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+            cell.imageOfPlace.clipsToBounds = true
+    
+            return cell
+        }
+    
     
     //MARK: - Table view delegate
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
+
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let place = places[indexPath.row]
+        let deleteAtcion = UITableViewRowAction(style: .default, title: "Delete") { _, _ in
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        return [deleteAtcion]
     }
 
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue){
+        guard let newPlaceVC = segue.source as? NewPlaceViewController else {return}
+        newPlaceVC.saveNewPlace()
+        
+        tableView.reloadData()
     }
-    */
-
 }
+
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,24 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Евгений Солохин on 12.09.2022.
+//
+
+import RealmSwift
+
+class Place: Object {
+    
+    @objc dynamic var name = ""
+    @objc dynamic var location: String?
+    @objc dynamic var type: String?
+    @objc dynamic var imageData: Data?
+    
+    convenience init(name: String, location: String?, type: String?, imageData: Data?){
+        self.init()
+        self.name = name
+        self.location = location
+        self.type = type
+        self.imageData = imageData
+    }
+}
